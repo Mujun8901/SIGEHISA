de4e073c2694935a3b9a0f9ec14e5169b34c94fd
重久裕幸 <1816216@s.asojuku.ac.jp>
2020-05-13 19:20:26 +0900

    プレイヤーの攻撃を追加最中

--- SteamDoll/Assets/Scripts/PlayerControl.cs
+++ SteamDoll/Assets/Scripts/PlayerControl.cs
@@ -10,33 +10,43 @@ public class PlayerControl : MonoBehaviour
     public float speed;
     public float jumpSpeed;
     public float gravity;
+    public float smooth;
+    private bool isGround;  // 地面との接触判定
 
-    CharacterController controller;
+    //CharacterController controller;
+    Rigidbody rb;
+    Animator animator;
 
     Vector3 moveDir;
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        // controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
         moveDir = Vector3.zero;
+        isGround = true;
     }
 
     void Update()
     {
-        if(controller.isGrounded)
+        if(isGround)
         {
+            RotatePlayer();
             MovePlayer();
             JumpPlayer();
         }
-        else
+        else if(!isGround)
         {
-            IsGravity();
+            //IsGravity();       
         }
 
         // 移動実行
-        Vector3 globalDirection = transform.TransformDirection(moveDir);
-        controller.Move(globalDirection * Time.deltaTime);
-        if (controller.isGrounded) moveDir.y = 0.0f;
+        rb.MovePosition(transform.position * Time.deltaTime + new Vector3(moveDir.z, moveDir.y, moveDir.z));
+        if (isGround) moveDir.y = 0.0f;
+
+        // アニメーション
+        animator.SetBool("jump", isGround);
     }
 
     void MovePlayer()
@@ -49,15 +59,36 @@ public class PlayerControl : MonoBehaviour
 
     void JumpPlayer()
     {
-        Debug.Log("jump");
+        // ジャンプ
         if (Input.GetButton("Jump"))
         {
             moveDir.y = jumpSpeed;
         }
     }
 
+    void RotatePlayer()
+    {
+        if (moveDir.magnitude > 0.1f)
+        {
+            Quaternion rotation = Quaternion.LookRotation(new Vector3(moveDir.x, 0, moveDir.z));
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * smooth);
+        }
+    }
+
     void IsGravity()
     {
         moveDir.y -= gravity * Time.deltaTime;
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("stay");
+        isGround = true;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        isGround = false;
+        Debug.Log("jump");
     }
 }
