df4df1677e12ece5bd65a1511ae621c4481d9c66
重久裕幸 <1816216@s.asojuku.ac.jp>
2020-05-20 18:11:55 +0900

    攻撃

--- SteamDoll/Assets/Scripts/EnemyControl.cs
+++ SteamDoll/Assets/Scripts/EnemyControl.cs
@@ -7,14 +7,22 @@ using UnityEngine.AI;
 
 public class EnemyControl : MonoBehaviour
 {
+    [SerializeField]
+    private float waitTime;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Transform muzzle;
     public Transform target;
+    public GameObject shot;
     public bool setWolk;
     private float enemySearchRadius;
     private Transform myTransform;
     public NavMeshAgent agent;
-
+    private bool nextAttack = false;
+    private float time = 0;
+    private int move = 0;
     private LayerMask raycastLayer;
-
 
     void Start()
     {
@@ -30,6 +38,8 @@ public class EnemyControl : MonoBehaviour
     {
         SearchForTorget();
         MoveForTorget();
+        RangeToTarget();
+        
     }
 
     public void SearchForTorget()
@@ -72,16 +82,110 @@ public class EnemyControl : MonoBehaviour
             Vector2 randomPos = Random.insideUnitCircle * 20;
             agent.destination = myPos + new Vector3(randomPos.x, 0, randomPos.y);
             setWolk = true;
-            Debug.Log("setWolk");
         }
         if (setWolk)
         {
-            Debug.Log("stop");
             agent.SetDestination(agent.destination);
-            if (agent.remainingDistance < 5) 
+            if (agent.remainingDistance < agent.stoppingDistance) 
             {
                 setWolk = false;
             }
         }
+    }
+    
+    void RangeToTarget()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere
+                           (myTransform.position, enemySearchRadius, raycastLayer);
+
+        if (agent.remainingDistance < agent.stoppingDistance && hitColliders.Length > 0)
+        {
+            int attack = 0;
+            attack = Random.Range(0, 3);
+            // 攻撃できるようになったら
+            if (AttackInterval())
+            {
+                if (attack == 0)
+                {
+                    Attack1();
+                }
+                else if (attack == 1)
+                {
+                    Attack2();
+                }
+                else if (attack == 2) 
+                {
+                    Attack3();
+                }
+            }
+            else
+            {
+                // インターバル中ランダムに動くか動かないか(0:動く　1:動かない)
+                SmoothLookAt();
+            }            
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    // 攻撃A 砲撃
+    void Attack1()
+    {
+        GameObject shots = Instantiate(shot) as GameObject;
+
+        shots.transform.position = muzzle.position;
+        
+        Debug.Log("1");
+        // 攻撃を終了する
+        nextAttack = false;
+    }
+
+    // 攻撃B 拡散弾
+    void Attack2()
+    {
+        GameObject shots = Instantiate(shot) as GameObject;
+
+        shots.transform.position = muzzle.position;
+        Debug.Log("2");
+        // 攻撃を終了する
+        nextAttack = false;
+    }
+
+    // 攻撃C 連射弾
+    void Attack3()
+    {
+        GameObject shots = Instantiate(shot) as GameObject;
+
+        shots.transform.position = muzzle.position;
+        Debug.Log("3");
+        // 攻撃を終了する
+        nextAttack = false;
+    }
+
+    // アニメーション(共通)    
+    // 攻撃のインターバル        
+    bool AttackInterval()
+    {
+        // 一定時間経ったら
+        if (time > waitTime && !nextAttack)
+        {
+            // 再度攻撃できる
+            nextAttack = true;
+            time = 0;
+        }
+        
+        // タイムカウント
+        time += Time.deltaTime;
+        return nextAttack;
+    }
+
+    void SmoothLookAt()
+    {
+        transform.rotation = Quaternion.Slerp(
+                        transform.rotation,
+                        Quaternion.LookRotation(target.position - transform.position),
+                        0.1f);
     }
 }
